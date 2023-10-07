f0a1fedb71e849a890811ad5f44abeab41841177
carlos-sardina <developerAlbertoMT@gmail.com>
2023-10-06 22:51:04 -0600

    adding custom matchers and fixing the build

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,25 +1,20 @@
 import '@testing-library/jest-dom';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '../../../plick-test-utils';
 import { Button } from './Button';
 
 describe('Button Component', () => {
   it('renders button text and responds to click event', () => {
     const buttonText = 'Click me';
-    const onClickMock = jest.fn(); // Mocking the onClick function
+    const onClickMock = jest.fn();
 
     const { getByText } = render(
       <Button text={buttonText} onClick={onClickMock} />
     );
 
     const buttonElement = getByText(buttonText);
-
-    // Assert that the button text is rendered
     expect(buttonElement).toBeInTheDocument();
 
-    // Simulate a click event on the button
     fireEvent.click(buttonElement);
-
-    // Assert that the onClick function was called
     expect(onClickMock).toHaveBeenCalledTimes(1);
   });
 });
---
+++ src/components/Title/Title.test.tsx
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom';
+import { render } from '../../../plick-test-utils';
+import { Title } from './Title';
+
+describe('Title Component', () => {
+  it('renders the title text correctly', () => {
+    const titleText = 'Hello, World!';
+
+    const { getByText } = render(<Title text={titleText} />);
+    const titleElement = getByText(titleText);
+
+    expect(titleElement).toBeInTheDocument();
+    expect(titleElement.tagName).toBe('H1');
+  });
+});
