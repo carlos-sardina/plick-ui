e0fca434cb8b0f9813ad06ed248c366020fc32e0
carlos-sardina <developerAlbertoMT@gmail.com>
2023-10-07 00:57:04 -0600

    test failed test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -11,7 +11,7 @@ describe('Button Component', () => {
     );
 
     const buttonElement = getByText(buttonText);
-    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).not.toBeInTheDocument();
 
     fireEvent.click(buttonElement);
     expect(onClickMock).toHaveBeenCalledTimes(1);
